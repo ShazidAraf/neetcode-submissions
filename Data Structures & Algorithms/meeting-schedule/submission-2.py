99ab4d9a625f27bd9c5e576ef06a84b3e1a5cdfe
MD SHAZID ISLAM <misla048@ucr.edu>
2026-09-05 07:22:30 -0700

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.py
@@ -0,0 +1,28 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+
+
+        intervals.sort(key = lambda x : x.start)
+
+        print(intervals)
+
+
+        for i in range(1,len(intervals)):
+
+            x = intervals[i-1]
+            y = intervals[i]
+
+            if x.end>y.start:
+                return False
+
+
+        return True
+
