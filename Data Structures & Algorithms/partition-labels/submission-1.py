0fb6f5a0a6d88c113bccf5b4ec445769e4cdebe5
MD SHAZID ISLAM <misla048@ucr.edu>
2026-09-12 08:24:28 -0700

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+
+        # H = defaultdict(list)
+        H = {}
+        # print(H)
+
+
+        for i in range(len(s)):
+
+            if H.get(s[i],0) == 0:
+                H[s[i]] = [i,i]
+
+            else:
+                H[s[i]][1] = i
+
+        intervals = list(H.values())
+        intervals.sort(key=lambda x: x[0])
+
+        res = []
+        start = 0
+        end = 0
+
+        for i in range(len(intervals)):
+            end = max(end, intervals[i][1])         
+            if i == len(intervals)-1 or end < intervals[i+1][0]:
+                res.append(end - start + 1)
+                start = end + 1
+
+        return res
+
+
+
+
+        
